a527ba99b17da6b25c654455ad2ea3c0f826f7d8
fleece30 <abhiyad25@gmail.com>
2026-07-11 02:35:36 -0700

    refactor: comment out non-necessary lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,8 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "pyright", "ts_ls" }
+				ensure_installed = { "lua_ls" }
+				-- ensure_installed = { "lua_ls", "gopls", "pyright", "ts_ls" }
 			})
 		end
 	},
